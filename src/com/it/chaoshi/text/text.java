1dc368cf503c2243358f6eb0dcd71180d338f06f
crazyma <2659321365@qq.com>
2022-04-14 17:35:03 +0800

    myl   第一次提交

--- src/com/it/chaoshi/text/text.java
+++ src/com/it/chaoshi/text/text.java
@@ -15,6 +15,7 @@ public class text {
 //        Usercontroller bean = context.getBean(Usercontroller.class);
 //        User user = bean.getUser(2);
 //        System.out.println(user);
+        System.out.println("11111111111111111111");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext..xml");
         UserServiceImpl bean = context.getBean(UserServiceImpl.class);
         Random random = new Random();
